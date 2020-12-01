8a60ffd53d6a262a7b1bcdaf78b475dbc8c1a349
Thomas Bouder <tbouder@protonmail.com>
2020-12-01 09:58:16 +0100

    🏷️ Exporting types

--- ts/index.ts
+++ ts/index.ts
@@ -4,54 +4,26 @@ import * as crypto from 'crypto'
 import * as ethers from 'ethers'
 import { convertWitness, prove, beBuff2int } from './utils' 
 import { storage, hashers, tree } from 'semaphore-merkle-tree'
+import {
+    Identity,
+    EddsaKeyPair,
+    EddsaPrivateKey,
+    EddsaPublicKey,
+    EdDSASignature,
+    SnarkCircuit,
+    SnarkProvingKey,
+    SnarkVerifyingKey ,
+    SnarkWitness,
+    SnarkPublicSignals,
+    SnarkProof,
+    SnarkBigInt,
+    WitnessData,
+} from './types';
 const MemStorage = storage.MemStorage
 const MerkleTree = tree.MerkleTree
 const MimcSpongeHasher = hashers.MimcSpongeHasher
 const stringifyBigInts: (obj: object) => object = snarkjs.stringifyBigInts
 const unstringifyBigInts: (obj: object) => object = snarkjs.unstringifyBigInts
-
-type SnarkBigInt = snarkjs.bigInt
-type EddsaPrivateKey = Buffer
-type EddsaPublicKey = SnarkBigInt[]
-type SnarkCircuit = snarkjs.Circuit
-type SnarkProvingKey = Buffer
-type SnarkVerifyingKey = Buffer
-type SnarkWitness = Array<SnarkBigInt>
-type SnarkPublicSignals = SnarkBigInt[]
-
-interface WitnessData {
-    witness: any,
-    signal: string,
-    signalHash: SnarkBigInt,
-    signature: EdDSASignature,
-    msg: SnarkBigInt,
-    tree: tree.MerkleTree,
-    identityPath: any,
-    identityPathIndex: any,
-    identityPathElements: any,
-}
-
-interface EddsaKeyPair {
-    pubKey: EddsaPublicKey,
-    privKey: EddsaPrivateKey,
-}
-
-interface Identity {
-    keypair: EddsaKeyPair,
-    identityNullifier: SnarkBigInt,
-    identityTrapdoor: SnarkBigInt,
-}
-
-interface SnarkProof {
-    pi_a: SnarkBigInt[]
-    pi_b: SnarkBigInt[][]
-    pi_c: SnarkBigInt[]
-}
-
-interface EdDSASignature {
-    R8: SnarkBigInt[],
-    S: SnarkBigInt,
-}
 
 const pedersenHash = (
     ints: SnarkBigInt[],
---
+++ ts/types.d.ts
@@ -0,0 +1,61 @@
+import * as snarkjs from 'snarkjs'
+import { tree } from 'semaphore-merkle-tree'
+
+type SnarkBigInt = snarkjs.bigInt
+type EddsaPrivateKey = Buffer
+type EddsaPublicKey = SnarkBigInt[]
+type SnarkCircuit = snarkjs.Circuit
+type SnarkProvingKey = Buffer
+type SnarkVerifyingKey = Buffer
+type SnarkWitness = Array<SnarkBigInt>
+type SnarkPublicSignals = SnarkBigInt[]
+
+interface WitnessData {
+    witness: any,
+    signal: string,
+    signalHash: SnarkBigInt,
+    signature: EdDSASignature,
+    msg: SnarkBigInt,
+    tree: tree.MerkleTree,
+    identityPath: any,
+    identityPathIndex: any,
+    identityPathElements: any,
+}
+
+interface EddsaKeyPair {
+    pubKey: EddsaPublicKey,
+    privKey: EddsaPrivateKey,
+}
+
+interface Identity {
+    keypair: EddsaKeyPair,
+    identityNullifier: SnarkBigInt,
+    identityTrapdoor: SnarkBigInt,
+}
+
+interface SnarkProof {
+    pi_a: SnarkBigInt[]
+    pi_b: SnarkBigInt[][]
+    pi_c: SnarkBigInt[]
+}
+
+interface EdDSASignature {
+    R8: SnarkBigInt[],
+    S: SnarkBigInt,
+}
+
+export {
+    Identity,
+    EddsaKeyPair,
+    EddsaPrivateKey,
+    EddsaPublicKey,
+    EdDSASignature,
+    SnarkCircuit,
+    SnarkProvingKey,
+    SnarkVerifyingKey ,
+    SnarkWitness,
+    SnarkPublicSignals,
+    SnarkProof,
+    SnarkBigInt,
+    WitnessData,
+}
